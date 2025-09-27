bcb59e8e4e9e136bfe9b771b737cb0f88cb64101
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-09-27 11:00:47 +0900

    escape filter

--- cmd/mdfm/jq.go
+++ cmd/mdfm/jq.go
@@ -10,11 +10,11 @@ import (
 func prepareJQ(filter string) (*gojq.Code, error) {
 	jqQuery, jqErr := gojq.Parse(filter)
 	if jqErr != nil {
-		return nil, fmt.Errorf("error parsing jq filter %s: %w", filter, jqErr)
+		return nil, fmt.Errorf("error parsing jq filter %q: %w", filter, jqErr)
 	}
 	jqCode, jqCompileErr := gojq.Compile(jqQuery, gojq.WithEnvironLoader(os.Environ))
 	if jqCompileErr != nil {
-		return nil, fmt.Errorf("error compiling jq filter %s: %w", filter, jqCompileErr)
+		return nil, fmt.Errorf("error compiling jq filter %q: %w", filter, jqCompileErr)
 	}
 
 	return jqCode, nil
